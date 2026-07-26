9f8afc7cbed04859294f6a08c80664b873808f09
Divyanshu Jaiswal <divyanshu.jaiswal.ece24@itbhu.ac.in>
2026-07-27 02:26:53 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int count(int n){
+        int cnt=0;
+        while(n){
+            cnt++;
+            n=n&(n-1);
+        }
+        return cnt;
+    }
+
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        for(int i=0;i<=n;i++){
+            ans[i]=count(i);
+        }
+        return ans;
+    }
+};
