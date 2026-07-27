6e41e4853b490ff85921afc13d96e6269d5cdb51
Divyanshu Jaiswal <divyanshu.jaiswal.ece24@itbhu.ac.in>
2026-07-27 17:00:54 +0530

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        int maxi=1;
+        vector<int> dp(n,1);
+        for(int ind=0;ind<n;ind++){
+            for(int prev=0;prev<ind;prev++){
+                if(nums[ind]>nums[prev]){
+                    dp[ind]=max(dp[ind],1+dp[prev]);
+                }
+            }
+            maxi=max(maxi,dp[ind]);
+        }   
+        return maxi;
+    }
+};
